ce92c1b0f253a8557dd0523fea6ec71b590675cd
jasminbecic <becicjasmin1@gmail.com>
2022-04-23 20:53:40 -0500

    initial commit

---
+++ src/main/java/org/launchcode/liftoffproject/LiftoffprojectApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.liftoffproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiftoffprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiftoffprojectApplication.class, args);
+	}
+
+}
