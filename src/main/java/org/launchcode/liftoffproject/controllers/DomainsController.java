4fdd4fdb974e1687f88b0c29f56f2db9ca9e21fa
jasminbecic <becicjasmin1@gmail.com>
2022-04-29 23:38:38 -0500

    added domains

---
+++ src/main/java/org/launchcode/liftoffproject/controllers/DomainsController.java
@@ -0,0 +1,45 @@
+package org.launchcode.liftoffproject.controllers;
+
+import org.launchcode.liftoffproject.data.DomainRepository;
+import org.launchcode.liftoffproject.data.InterventionRepository;
+import org.launchcode.liftoffproject.models.Domain;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
+
+@Controller
+@RequestMapping("domains")
+public class DomainsController {
+
+    @Autowired
+    private DomainRepository domainRepository;
+
+    @Autowired
+    private InterventionRepository interventionRepository;
+
+    @GetMapping
+    public String displayAllDomains(Model model) {
+        model.addAttribute("title", "All Domains");
+        model.addAttribute("domains", domainRepository.findAll());
+
+        return "domains/index";
+    }
+
+    @GetMapping("view/{domainId}")
+    public String displayViewDomain(Model model, @PathVariable int domainId) {
+        Optional optDomain = domainRepository.findById(domainId);
+        if (optDomain.isPresent()) {
+            Domain domain = (Domain) optDomain.get();
+            model.addAttribute("domain", domain);
+            model.addAttribute("interventions", interventionRepository.findById(domainId));
+            return "domains/view";
+        } else {
+            return "redirect:../";
+        }
+    }
+}
