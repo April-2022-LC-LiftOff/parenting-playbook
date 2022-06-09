77b068a66050e97ef9ad535cee62849c33594db3
jasminbecic <becicjasmin1@gmail.com>
2022-06-09 15:11:11 -0500

    added more controllers to reduce dependence on HomeController

--- src/main/java/org/launchcode/liftoffproject/controllers/HomeController.java
+++ src/main/java/org/launchcode/liftoffproject/controllers/HomeController.java
@@ -1,19 +1,17 @@
 package org.launchcode.liftoffproject.controllers;
 
-import org.launchcode.liftoffproject.data.*;
-import org.launchcode.liftoffproject.models.*;
+import org.launchcode.liftoffproject.data.DomainRepository;
+import org.launchcode.liftoffproject.data.InterventionRepository;
+import org.launchcode.liftoffproject.data.TagRepository;
+import org.launchcode.liftoffproject.models.HelperMethods;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.validation.Valid;
-import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.io.FileNotFoundException;
 
 @Controller
 public class HomeController {
@@ -28,16 +26,7 @@ public class HomeController {
     private TagRepository tagRepository;
 
     @Autowired
-    private CommentRepository commentRepository;
-
-    @Autowired
     private AuthenticationController authenticationController;
-
-    @Autowired
-    private UserRepository userRepository;
-
-
-
 
     @RequestMapping("")
     public String index(Model model, HttpServletRequest request) throws FileNotFoundException {
@@ -53,118 +42,6 @@ public class HomeController {
         return "index";
     }
 
-    @GetMapping("add")
-    public String displayAddInterventionForm(Model model, HttpServletRequest request) {
-        model.addAttribute("loggedIn", authenticationController.isUserLoggedIn(request));
-
-        if (authenticationController.isUserLoggedIn(request)) {
-            model.addAttribute("title", "Add Intervention");
-            model.addAttribute(new Intervention());
-            model.addAttribute("domains", domainRepository.findAll());
-            model.addAttribute("tags", tagRepository.findAll());
-            User user = new User();
-            model.addAttribute("username", user.getUsername());
-            model.addAttribute(user);
-
-            return "add";
-        }
-        return "redirect:";
-    }
-
-    @PostMapping("add")
-    public String processAddInterventionForm(@ModelAttribute @Valid Intervention newIntervention, Errors errors, Model model, @RequestParam(required = false) List<Integer> domains, @RequestParam(required = false) List<Integer> tag, HttpServletRequest request) {
-        User user = authenticationController.getUserFromSession(request.getSession());
-        model.addAttribute("loggedIn", authenticationController.isUserLoggedIn(request));
-
-        if (domains == null || domains.size() == 0 || domains.isEmpty()) {
-            model.addAttribute("title", "Add Intervention");
-            model.addAttribute("domains", domainRepository.findAll());
-            model.addAttribute("tags", tagRepository.findAll());
-            String str = "A Domain must be selected.";
-            model.addAttribute("checkBoxError", str);
-            return "add";
-        }
-
-        if (tag == null || tag.size() == 0 || tag.isEmpty()) {
-            model.addAttribute("title", "Add Intervention");
-            model.addAttribute("domains", domainRepository.findAll());
-            model.addAttribute("tags", tagRepository.findAll());
-            String str = "A Tag must be selected.";
-            model.addAttribute("checkBoxErrorTag", str);
-            return "add";
-        }
-
-        if (errors.hasErrors()) {
-            model.addAttribute("title", "Add Intervention");
-            model.addAttribute("domains", domainRepository.findAll());
-            model.addAttribute("tags", tagRepository.findAll());
-            model.addAttribute("user", userRepository.findAll());
-            return "add";
-        }
-
-        List<Domain> domainObjs = (List<Domain>) domainRepository.findAllById(domains);
-        List<Tag> tagObjs = (List<Tag>) tagRepository.findAllById(tag);
-
-        newIntervention.setUser(user);
-        newIntervention.setDomains(domainObjs);
-        newIntervention.setTags(tagObjs);
-
-        interventionRepository.save(newIntervention);
-
-        return "redirect:/profile";
-    }
-
-    @GetMapping("view/{interventionId}")
-    public String displayViewIntervention(Model model, @PathVariable int interventionId, HttpServletRequest request) {
-        Optional optIntervention = interventionRepository.findById(interventionId);
-        if (optIntervention.isPresent()) {
-            Intervention intervention = (Intervention) optIntervention.get();
-            model.addAttribute("intervention", intervention);
-            model.addAttribute("detectURL", HelperMethods.detectURL(intervention.getReference()));
-            if (HelperMethods.detectURL(intervention.getReference())) {
-                String clickableURL = HelperMethods.clickableURL(intervention.getReference());
-                model.addAttribute("clickableURL", clickableURL);
-            }
-
-            User user = authenticationController.getUserFromSession(request.getSession());
-            model.addAttribute("loggedIn", authenticationController.isUserLoggedIn(request));
-            Comment comment = new Comment();
-
-            if (user != null) {
-                model.addAttribute("comments", commentRepository.findCommentByInterventionIdAndUserId(interventionId, user.getId()));
-                if (user == intervention.getUser()) {
-                    model.addAttribute("initialUser", true);
-                }
-            }
-
-            model.addAttribute("comment", comment);
-            model.addAttribute("user", user);
-
-            return "view";
-        } else {
-            return "redirect:../";
-        }
-    }
-
-    @PostMapping("view/{interventionId}")
-    public String processAddComment(@ModelAttribute @Valid Comment newComment, Errors errors, Model model,
-                                    @PathVariable int interventionId, HttpServletRequest request) {
-        User user = authenticationController.getUserFromSession(request.getSession());
-        model.addAttribute("loggedIn", authenticationController.isUserLoggedIn(request));
-
-        Optional optIntervention = interventionRepository.findById(interventionId);
-        Intervention intervention = (Intervention) optIntervention.get();
-        if (errors.hasErrors()) {
-            model.addAttribute("intervention", intervention);
-            return "view";
-        }
-
-        newComment.setUser(user);
-        newComment.setIntervention(intervention);
-        commentRepository.save(newComment);
-        return "redirect:{interventionId}";
-    }
-
 
     @GetMapping("about")
     public String displayAbout(Model model, HttpServletRequest request) {
@@ -177,94 +54,5 @@ public class HomeController {
         model.addAttribute("loggedIn", authenticationController.isUserLoggedIn(request));
         return "faq";
     }
-
-    List<String> quizResults = new ArrayList<>();
-
-    @GetMapping("quiz")
-    public String displayAllQuestions(Model model, HttpServletRequest request) {
-        model.addAttribute("loggedIn", authenticationController.isUserLoggedIn(request));
-        List<String> questionnaire = new ArrayList<>();
-        model.addAttribute("questionnaire", questionnaire);
-        return "quiz";
-    }
-
-    @GetMapping("results")
-    public String getResults(Model model, Quiz quiz, HttpServletRequest request) {
-        model.addAttribute("loggedIn", authenticationController.isUserLoggedIn(request));
-        model.addAttribute("Quiz", quiz);
-        return "results";
-    }
-
-    @PostMapping("results")
-    public String processFormMethodQuiz(@ModelAttribute("quiz") @Valid Quiz quiz, Model model, @RequestParam(required = false)
-            List<String> impulseControl, @RequestParam(required = false) List<String> emotionalControl, @RequestParam(required = false)
-                                                List<String> flexibleThinking, @RequestParam(required = false)
-                                                List<String> workingMemory, @RequestParam(required = false)
-                                                List<String> selfMonitoring, @RequestParam(required = false)
-                                                List<String> planningAndPrioritizing, @RequestParam(required = false)
-                                                List<String> taskInitiation, @RequestParam(required = false)
-                                                List<String> organization, @RequestParam(required = false) List<String> none, HttpServletRequest request) {
-
-        model.addAttribute("loggedIn", authenticationController.isUserLoggedIn(request));
-
-        if (impulseControl == null || impulseControl.size() < 2) {
-            model.addAttribute("title", quiz);
-        } else if (impulseControl.size() >= 2) {
-            model.addAttribute("impulseControl", domainRepository.findById(1));
-        }
-
-        if (emotionalControl == null || emotionalControl.size() < 2) {
-            model.addAttribute("title", quiz);
-        } else if (emotionalControl.size() >= 2) {
-            model.addAttribute("emotionalControl", domainRepository.findById(2));
-        }
-
-        if (flexibleThinking == null || flexibleThinking.size() < 2) {
-            model.addAttribute("title", quiz);
-        } else if (flexibleThinking.size() >= 2) {
-            model.addAttribute("flexibleThinking", domainRepository.findById(3));
-        }
-
-        if (workingMemory == null || workingMemory.size() < 2) {
-            model.addAttribute("title", quiz);
-        } else if (workingMemory.size() >= 2) {
-            model.addAttribute("workingMemory", domainRepository.findById(4));
-        }
-
-        if (selfMonitoring == null || selfMonitoring.size() < 2) {
-            model.addAttribute("title", quiz);
-        } else if (selfMonitoring.size() >= 2) {
-            model.addAttribute("selfMonitoring", domainRepository.findById(5));
-        }
-
-        if (planningAndPrioritizing == null || planningAndPrioritizing.size() < 2) {
-            model.addAttribute("title", quiz);
-        } else if (planningAndPrioritizing.size() >= 2) {
-            model.addAttribute("planningAndPrioritizing", domainRepository.findById(6));
-        }
-
-        if (taskInitiation == null || taskInitiation.size() < 2) {
-            model.addAttribute("title", quiz);
-        } else if (taskInitiation.size() >= 2) {
-            model.addAttribute("taskInitiation", domainRepository.findById(7));
-        }
-
-        if (organization == null || organization.size() < 2) {
-            model.addAttribute("title", quiz);
-        } else if (organization.size() >= 2) {
-            model.addAttribute("organization", domainRepository.findById(8));
-        }
-
-        if ((impulseControl == null || impulseControl.size() < 2) && (emotionalControl == null || emotionalControl.size() < 2)
-                && (flexibleThinking == null || flexibleThinking.size() < 2) && (workingMemory == null || workingMemory.size() < 2)
-                && (selfMonitoring == null || selfMonitoring.size() < 2) && (planningAndPrioritizing == null || planningAndPrioritizing.size() < 2)
-                && (taskInitiation == null || taskInitiation.size() < 2) && (organization == null || organization.size() < 2)) {
-            model.addAttribute("title", quiz);
-            model.addAttribute("none", domainRepository.findAll());
-
-        }
-        return "results";
-    }
-
 
 }
